e924ba277c82bee5110c22b9bd3683fb34aca767
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-01 10:36:51 +0800

    Update T5.java

--- clrs/src/leetcode/dp/T5.java
+++ clrs/src/leetcode/dp/T5.java
@@ -14,6 +14,32 @@ package leetcode.dp;
  */
 public class T5 {
     public String longestPalindrome(String s) {
-
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        int start = 0;
+        int end = 0;
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n; i++) {
+                int j = len + i - 1;
+                if (j >= n) break;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len <= 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+                if (dp[i][j] && len > end - start + 1) {
+                    start = i;
+                    end = j;
+                }
+            }
+        }
+        return s.substring(start, end + 1);
     }
 }
