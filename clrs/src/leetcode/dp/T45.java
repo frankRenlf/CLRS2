d2377182c500f7ed442c7286c8b88acb71a25f46
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-01 11:00:30 +0800

    Update T45.java

--- clrs/src/leetcode/dp/T45.java
+++ clrs/src/leetcode/dp/T45.java
@@ -9,11 +9,22 @@ package leetcode.dp;
  * @Package : leetcode.dp
  * @createTime : 2023/3/1 10:54
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T45 {
     public int jump(int[] nums) {
-        
+        int n = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
     }
 }
