92cd58866ad4aaecb9211cb32edae80b1f6c21a9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-04 09:14:01 +0800

    update

---
+++ clrs/src/leetcode/dp/T95.java
@@ -0,0 +1,23 @@
+package leetcode.dp;
+
+import leetcode.algorithm.TreeNode;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/4 9:09
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T95 {
+    public List<TreeNode> generateTrees(int n) {
+
+    }
+}
