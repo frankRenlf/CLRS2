f6667c152704a660b3efc29055a5d89f47027c03
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-07 09:45:41 +0800

    Create T1096.java

---
+++ clrs/src/leetcode/daily/T1096.java
@@ -0,0 +1,79 @@
+package leetcode.daily;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/3/7 9:45
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1096 {
+    String expression;
+    int idx;
+
+    public List<String> braceExpansionII(String expression) {
+        this.expression = expression;
+        this.idx = 0;
+        Set<String> ret = expr();
+        return new ArrayList<String>(ret);
+    }
+
+    // item . letter | { expr }
+    private Set<String> item() {
+        Set<String> ret = new TreeSet<String>();
+        if (expression.charAt(idx) == '{') {
+            idx++;
+            ret = expr();
+        } else {
+            ret.add(String.valueOf(expression.charAt(idx)));
+        }
+        idx++;
+        return ret;
+    }
+
+    // term . item | item term
+    private Set<String> term() {
+        // 初始化空集合，与之后的求解结果求笛卡尔积
+        Set<String> ret = new TreeSet<String>() {{
+            add("");
+        }};
+        // item 的开头是 { 或小写字母，只有符合时才继续匹配
+        while (idx < expression.length() && (expression.charAt(idx) == '{' || Character.isLetter(expression.charAt(idx)))) {
+            Set<String> sub = item();
+            Set<String> tmp = new TreeSet<String>();
+            for (String left : ret) {
+                for (String right : sub) {
+                    tmp.add(left + right);
+                }
+            }
+            ret = tmp;
+        }
+        return ret;
+    }
+
+    // expr . term | term, expr
+    private Set<String> expr() {
+        Set<String> ret = new TreeSet<String>();
+        while (true) {
+            // 与 term() 求解结果求并集
+            ret.addAll(term());
+            // 如果匹配到逗号则继续，否则结束匹配
+            if (idx < expression.length() && expression.charAt(idx) == ',') {
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return ret;
+    }
+}
