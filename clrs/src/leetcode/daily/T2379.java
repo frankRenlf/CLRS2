9fc0349ca1d3c77b145e1367c766fafbf03ef93a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-09 09:45:01 +0800

    Update T2379.java

--- clrs/src/leetcode/daily/T2379.java
+++ clrs/src/leetcode/daily/T2379.java
@@ -13,4 +13,24 @@ package leetcode.daily;
  * @Description :
  */
 public class T2379 {
+    public int minimumRecolors(String blocks, int k) {
+        int min = Integer.MAX_VALUE, cur = 0;
+        int n = blocks.length();
+        for (int i = 0, j = 0; i <= n - k; i++) {
+            if (j == n) {
+                break;
+            }
+            while (j <= i + k - 1 && j < n) {
+                if (blocks.charAt(j) == 'W') {
+                    cur++;
+                }
+                j++;
+            }
+            min = Math.min(min, cur);
+            if (blocks.charAt(i) == 'W') {
+                cur--;
+            }
+        }
+        return min;
+    }
 }
