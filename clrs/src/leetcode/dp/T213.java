2a8dc96301ae269666c66b99d79634748189f9b6
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-09 18:46:42 +0800

    Update T213.java

--- clrs/src/leetcode/dp/T213.java
+++ clrs/src/leetcode/dp/T213.java
@@ -14,6 +14,23 @@ package leetcode.dp;
  */
 public class T213 {
     public int rob(int[] nums) {
-
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp0 = new int[n];
+        int[] dp1 = new int[n];
+        dp0[0] = nums[0];
+        for (int i = 2; i < n - 1; i++) {
+            dp0[i] = Math.max(dp0[i - 2] + nums[i], dp0[i - 1]);
+        }
+        dp1[1] = nums[1];
+        for (int i = 3; i < n; i++) {
+            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
+        }
+        return Math.max(dp0[n - 2], dp1[n - 1]);
     }
+
 }
