27171b2ec3baa817f6b30e63cd75b27c79b27d67
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-09 20:46:23 +0800

    Update T300.java

--- clrs/src/leetcode/dp/T300.java
+++ clrs/src/leetcode/dp/T300.java
@@ -1,5 +1,7 @@
 package leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +16,18 @@ package leetcode.dp;
  */
 public class T300 {
     public int lengthOfLIS(int[] nums) {
-        
+        int n = nums.length;
+        int[] dp = new int[n];
+        int max = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
     }
 }
