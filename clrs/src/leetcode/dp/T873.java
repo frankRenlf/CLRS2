2adb09f44be966d6ef71ab11331c6be5b30a5087
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-10 16:08:15 +0800

    Create T873.java

---
+++ clrs/src/leetcode/dp/T873.java
@@ -0,0 +1,38 @@
+package leetcode.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/10 16:02
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T873 {
+    public int lenLongestFibSubseq(int[] arr) {
+        Map<Integer, Integer> indices = new HashMap<Integer, Integer>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            indices.put(arr[i], i);
+        }
+        int[][] dp = new int[n][n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+                int k = indices.getOrDefault(arr[i] - arr[j], -1);
+                if (k >= 0) {
+                    dp[j][i] = Math.max(dp[k][j] + 1, 3);
+                }
+                ans = Math.max(ans, dp[j][i]);
+            }
+        }
+        return ans;
+    }
+}
