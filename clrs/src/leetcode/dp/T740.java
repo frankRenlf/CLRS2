c3e037db063271b41e6cda5e6ffa1774c0d02e20
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-10 14:36:25 +0800

    Update T740.java

--- clrs/src/leetcode/dp/T740.java
+++ clrs/src/leetcode/dp/T740.java
@@ -9,7 +9,7 @@ package leetcode.dp;
  * @Package : leetcode.dp
  * @createTime : 2023/3/10 14:34
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T740 {
