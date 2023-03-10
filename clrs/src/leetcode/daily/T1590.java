b0cd25a839eb23cd2223eef1aa47dff66eb6e507
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-10 10:50:57 +0800

    update

---
+++ clrs/src/leetcode/daily/T1590.java
@@ -0,0 +1,46 @@
+package leetcode.daily;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/3/10 10:37
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1590 {
+    public int minSubarray(int[] nums, int p) {
+        int x = 0;
+        for (int num : nums) {
+            x = (x + num) % p;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> index = new HashMap<Integer, Integer>();
+        int y = 0, res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            index.put(y, i); // f[i] mod p = y，因此哈希表记录 y 对应的下标为 i
+            y = (y + nums[i]) % p;
+            if (index.containsKey((y - x + p) % p)) {
+                res = Math.min(res, i - index.get((y - x + p) % p) + 1);
+            }
+        }
+        return res == nums.length ? -1 : res;
+    }
+
+    static T1590 t1590 = new T1590();
+
+    public static void main(String[] args) {
+        t1590.minSubarray(new int[]{6, 3, 5, 2}, 9);
+    }
+
+}
