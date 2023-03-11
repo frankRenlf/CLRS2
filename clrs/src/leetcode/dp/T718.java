9c49ff434962df7989344c8fb74c254361f2c8e3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-11 10:34:07 +0800

    Create T718.java

---
+++ clrs/src/leetcode/dp/T718.java
@@ -0,0 +1,28 @@
+package leetcode.dp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/11 10:33
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T718 {
+    public int findLength(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                dp[i][j] = A[i] == B[j] ? dp[i + 1][j + 1] + 1 : 0;
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+}
