a5aa3cd3ea418b6c10190fea778bcc1b388b0fad
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-13 09:49:26 +0800

    Create T2383.java

---
+++ clrs/src/leetcode/daily/T2383.java
@@ -0,0 +1,19 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/3/13 9:48
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2383 {
+    public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        
+    }
+}
