dd9c2271ef8822d2c5df04d22e5a91f336f737d9
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-13 10:02:59 +0800

    Update T2383.java

--- clrs/src/leetcode/daily/T2383.java
+++ clrs/src/leetcode/daily/T2383.java
@@ -1,5 +1,7 @@
 package leetcode.daily;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -9,11 +11,24 @@ package leetcode.daily;
  * @Package : leetcode.daily
  * @createTime : 2023/3/13 9:48
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T2383 {
     public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
-        
+        int sum = Arrays.stream(energy).sum();
+        int add = 0;
+        for (int j : experience) {
+            if (j >= initialExperience) {
+                int sub = j - initialExperience + 1;
+                add += sub;
+                initialExperience += sub;
+            }
+            initialExperience += j;
+        }
+        if (initialEnergy <= sum) {
+            add += sum - initialEnergy + 1;
+        }
+        return add;
     }
 }
