d016a3dc5897ea3c2ebfe526a1bae5fc671e1722
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-14 10:04:46 +0800

    Create T1605.java

---
+++ clrs/src/leetcode/daily/T1605.java
@@ -0,0 +1,34 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/3/14 10:04
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1605 {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int n = rowSum.length, m = colSum.length;
+        int[][] matrix = new int[n][m];
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            int v = Math.min(rowSum[i], colSum[j]);
+            matrix[i][j] = v;
+            rowSum[i] -= v;
+            colSum[j] -= v;
+            if (rowSum[i] == 0) {
+                ++i;
+            }
+            if (colSum[j] == 0) {
+                ++j;
+            }
+        }
+        return matrix;
+    }
+}
