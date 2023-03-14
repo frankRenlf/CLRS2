0a55475f655efe29a2387efe14f7158bc2512ab1
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-14 11:27:14 +0800

    Update T746.java

--- clrs/src/leetcode/dp/T746.java
+++ clrs/src/leetcode/dp/T746.java
@@ -18,7 +18,7 @@ public class T746 {
         int[] dp = new int[n + 1];
         dp[0] = dp[1] = 0;
         for (int i = 2; i <= n; i++) {
-            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
         return dp[n];
     }
