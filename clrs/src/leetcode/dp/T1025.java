caf5257f022b8d6e4c0dace4699ed3fbb7e7f727
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-14 16:42:41 +0800

    Update T1025.java

--- clrs/src/leetcode/dp/T1025.java
+++ clrs/src/leetcode/dp/T1025.java
@@ -18,19 +18,12 @@ import java.util.List;
 public class T1025 {
     public boolean divisorGame(int n) {
         int end = n / 2;
-        boolean[] dp = new boolean[n + 1];
+        boolean[] dp = new boolean[n + 3];
         dp[1] = false;
         dp[2] = true;
-        dp[3] = false;
-        List<Integer> div = new ArrayList<>();
-        for (int i = 1; i <= end; i++) {
-            if (n % i == 0) {
-                div.add(i);
-            }
-        }
-        for (int i = 4; i <= n; i++) {
+        for (int i = 3; i <= n; i++) {
             for (int j = 1; j < i; j++) {
-                if (div.contains(j) && dp[i - j]) {
+                if ((i % j == 0) && !dp[i - j]) {
                     dp[i] = true;
                     break;
                 }
