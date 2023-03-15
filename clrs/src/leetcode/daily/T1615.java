51e571ead549f35ab4f2dbb3e6f6d1597bc2c014
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-15 10:13:00 +0800

    Update T1615.java

--- clrs/src/leetcode/daily/T1615.java
+++ clrs/src/leetcode/daily/T1615.java
@@ -1,5 +1,9 @@
 package leetcode.daily;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +18,22 @@ package leetcode.daily;
  */
 public class T1615 {
     public int maximalNetworkRank(int n, int[][] roads) {
+        boolean[][] connect = new boolean[n][n];
+        int[] degree = new int[n];
+        for (int[] v : roads) {
+            connect[v[0]][v[1]] = true;
+            connect[v[1]][v[0]] = true;
+            degree[v[0]]++;
+            degree[v[1]]++;
+        }
 
+        int maxRank = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int rank = degree[i] + degree[j] - (connect[i][j] ? 1 : 0);
+                maxRank = Math.max(maxRank, rank);
+            }
+        }
+        return maxRank;
     }
 }
