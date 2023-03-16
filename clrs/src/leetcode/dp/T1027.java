bfe306dfa3cd3bf4f705c3c14ff2afc6b5c45a18
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-16 12:00:48 +0800

    update

--- clrs/src/leetcode/dp/T1027.java
+++ clrs/src/leetcode/dp/T1027.java
@@ -1,5 +1,7 @@
 package leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +16,16 @@ package leetcode.dp;
  */
 public class T1027 {
     public int longestArithSeqLength(int[] nums) {
-
+        int n = nums.length;
+        int max = 0;
+        int[][] dp = new int[n][1001];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int d = nums[i] - nums[j] + 500;
+                dp[i][d] = dp[j][d] + 1;
+                max = Math.max(max, dp[i][d]);
+            }
+        }
+        return max + 1;
     }
 }
