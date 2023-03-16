01fc2140d6891f5fdaf62a5126b95e0f90d8dcca
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-16 16:59:57 +0800

    optimize space

--- clrs/src/leetcode/dp/T1027.java
+++ clrs/src/leetcode/dp/T1027.java
@@ -18,10 +18,11 @@ public class T1027 {
     public int longestArithSeqLength(int[] nums) {
         int n = nums.length;
         int max = 0;
-        int[][] dp = new int[n][1001];
+        int sub = Arrays.stream(nums).max().getAsInt() - Arrays.stream(nums).min().getAsInt();
+        int[][] dp = new int[n][sub * 2 + 1];
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                int d = nums[i] - nums[j] + 500;
+                int d = nums[i] - nums[j] + sub;
                 dp[i][d] = dp[j][d] + 1;
                 max = Math.max(max, dp[i][d]);
             }
