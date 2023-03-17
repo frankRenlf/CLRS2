09ef34eb8a840c98dde50db22e77b644ff614736
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-17 11:08:42 +0800

    u

--- clrs/src/leetcode/dp/O10_2.java
+++ clrs/src/leetcode/dp/O10_2.java
@@ -14,6 +14,17 @@ package leetcode.dp;
  */
 public class O10_2 {
     public int numWays(int n) {
-
+        if (n == 0) {
+            return 1;
+        } else if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
     }
 }
---
+++ clrs/src/leetcode/dp/O42.java
@@ -0,0 +1,19 @@
+package leetcode.dp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/17 11:08
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class O42 {
+    public int maxSubArray(int[] nums) {
+
+    }
+}
