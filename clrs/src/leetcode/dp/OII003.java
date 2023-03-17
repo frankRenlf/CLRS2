e0ec7f76dd4297a95389f5b61248cf4e52b1fd07
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-17 14:32:45 +0800

    Update OII003.java

--- clrs/src/leetcode/dp/OII003.java
+++ clrs/src/leetcode/dp/OII003.java
@@ -14,6 +14,14 @@ package leetcode.dp;
  */
 public class OII003 {
     public int[] countBits(int n) {
-
+        int[] dp = new int[n + 1];
+        int high = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                high = i;
+            }
+            dp[i] = dp[i - high] + 1;
+        }
+        return dp;
     }
 }
