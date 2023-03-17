eed7ac678453c868303997503c32ffe9e59fa455
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-17 10:57:36 +0800

    Create T1646.java

---
+++ clrs/src/leetcode/dp/T1646.java
@@ -0,0 +1,29 @@
+package leetcode.dp;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/17 10:57
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1646 {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] nums = new int[n + 1];
+        nums[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            nums[i] = nums[i / 2] + i % 2 * nums[i / 2 + 1];
+        }
+        return Arrays.stream(nums).max().getAsInt();
+    }
+}
