dacbed2eaa492c3b503c8b48ebd7a4276e625c62
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-17 11:03:45 +0800

    u

--- clrs/src/leetcode/dp/O10_1.java
+++ clrs/src/leetcode/dp/O10_1.java
@@ -14,6 +14,14 @@ package leetcode.dp;
  */
 public class O10_1 {
     public int fib(int n) {
-
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i < n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+        }
+        return dp[n];
     }
 }
