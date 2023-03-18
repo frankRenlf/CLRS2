ba34c84e14c9bf0ff482bcb6d7b1ff307716a58a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-18 11:36:52 +0800

    Create F17_16.java

---
+++ clrs/src/leetcode/dp/F17_16.java
@@ -0,0 +1,28 @@
+package leetcode.dp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/18 11:30
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class F17_16 {
+    public int massage(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        int[] dp = new int[n + 1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[n - 1];
+    }
+}
