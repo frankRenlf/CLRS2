c151d9a9bc3480fa291f442fece69af3b6188965
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-19 20:27:39 +0800

    Create T264.java

---
+++ clrs/src/leetcode/dp/T264.java
@@ -0,0 +1,19 @@
+package leetcode.dp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/19 20:27
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T264 {
+    public int nthUglyNumber(int n) {
+
+    }
+}
