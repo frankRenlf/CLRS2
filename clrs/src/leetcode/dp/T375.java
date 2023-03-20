5d104f9647dbb07382b2ed5eb6c3472bd6a609d0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-20 11:27:01 +0800

    Create T375.java

---
+++ clrs/src/leetcode/dp/T375.java
@@ -0,0 +1,28 @@
+package leetcode.dp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/20 11:26
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T375 {
+    public int getMoneyAmount(int n) {
+        int[][] f = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                f[i][j] = j + f[i][j - 1];
+                for (int k = i; k < j; k++) {
+                    f[i][j] = Math.min(f[i][j], k + Math.max(f[i][k - 1], f[k + 1][j]));
+                }
+            }
+        }
+        return f[1][n];
+    }
+}
