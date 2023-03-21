fc6e16cb3d08bb5db060e50683e1a49b255ccb6f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-21 10:03:09 +0800

    Update T714.java

--- clrs/src/leetcode/dp/T714.java
+++ clrs/src/leetcode/dp/T714.java
@@ -9,9 +9,20 @@ package leetcode.dp;
  * @Package : leetcode.dp
  * @createTime : 2023/3/21 9:47
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T714 {
-    
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        // 0 buy 1 sell
+        dp[0][0] = -prices[0] - fee;
+        dp[0][1] = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[n - 1][1];
+    }
 }
