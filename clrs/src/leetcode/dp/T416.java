b57eabb998dc3eb14509d1d67a158425776c474c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-25 11:02:51 +0800

    Create T416.java

---
+++ clrs/src/leetcode/dp/T416.java
@@ -0,0 +1,19 @@
+package leetcode.dp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.dp
+ * @createTime : 2023/3/25 11:02
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T416 {
+    public boolean canPartition(int[] nums) {
+
+    }
+}
