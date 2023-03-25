20241e5bb68c8be4e1fa7eca9a1ad40ce5c05701
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-25 11:15:58 +0800

    Update T416.java

--- clrs/src/leetcode/dp/T416.java
+++ clrs/src/leetcode/dp/T416.java
@@ -1,5 +1,7 @@
 package leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,7 +15,26 @@ package leetcode.dp;
  * @Description :
  */
 public class T416 {
-    public boolean canPartition(int[] nums) {
-
+    public boolean canPartition_subarray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) return false;
+        int half = sum / 2;
+        int l = 0, r = 0; // 0,l-1 l,r-1 r,n-1
+        int cur = 0;
+        while (l < n && r <= n) {
+            if (cur < half) {
+                if(r==n)return false;
+                r++;
+                cur += nums[r - 1];
+            } else if (cur > half) {
+                cur -= nums[l];
+                l++;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
