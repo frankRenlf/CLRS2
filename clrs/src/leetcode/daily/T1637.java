c09fd9836b90dcbaaa5f057a27b55308183eb7f2
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-30 11:39:56 +0800

    Create T1637.java

---
+++ clrs/src/leetcode/daily/T1637.java
@@ -0,0 +1,27 @@
+package leetcode.daily;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/3/30 11:38
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1637 {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> a[0] - b[0]);
+        int mx = 0;
+        for (int i = 1; i < points.length; i++) {
+            mx = Math.max(points[i][0] - points[i - 1][0], mx);
+        }
+        return mx;
+    }
+
+}
