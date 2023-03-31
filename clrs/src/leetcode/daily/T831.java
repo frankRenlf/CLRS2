bcdb650a3403cb1e14fbdc4e2c62de2b3a602e36
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-01 06:43:45 +0800

    complete email

--- clrs/src/leetcode/daily/T831.java
+++ clrs/src/leetcode/daily/T831.java
@@ -1,5 +1,7 @@
 package leetcode.daily;
 
+import java.util.Locale;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -9,11 +11,29 @@ package leetcode.daily;
  * @Package : leetcode.daily
  * @createTime : 2023/4/1 6:37
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T831 {
     public String maskPII(String s) {
+        if (s.contains("@")) {
+            return dealEmail(s);
+        }
+        return dealNumber(s);
+    }
 
+    private String dealNumber(String s) {
+        return null;
+    }
+
+    private String dealEmail(String s) {
+        s = s.toLowerCase(Locale.ROOT);
+        return s.charAt(0) + "*****" + s.substring(s.indexOf("@") - 1);
+    }
+
+    static T831 t831 = new T831();
+
+    public static void main(String[] args) {
+        System.out.println(t831.maskPII("AB@qq.com"));
     }
 }
