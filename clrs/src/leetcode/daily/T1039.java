8ea82a59896e0d6e65aac00bed7a5f3c585cb1ce
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-02 08:50:00 +0800

    Create T1039.java

---
+++ clrs/src/leetcode/daily/T1039.java
@@ -0,0 +1,17 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/2 8:49
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1039 {
+    
+}
