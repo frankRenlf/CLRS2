d4bc5016441de0f949f143b8ba123a68c6381701
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-02 08:50:54 +0800

    Update T1039.java

--- clrs/src/leetcode/daily/T1039.java
+++ clrs/src/leetcode/daily/T1039.java
@@ -1,5 +1,7 @@
 package leetcode.daily;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -9,9 +11,30 @@ package leetcode.daily;
  * @Package : leetcode.daily
  * @createTime : 2023/4/2 8:49
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
- * @Description :
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description : two methods, one dfs , one dp
  */
 public class T1039 {
-    
+    private int[] v;
+    private int[][] memo;
+
+    public int minScoreTriangulation(int[] values) {
+        v = values;
+        int n = v.length;
+        memo = new int[n][n];
+        for (int i = 0; i < n; ++i)
+            Arrays.fill(memo[i], -1); // -1 表示没有访问过
+        return dfs(0, n - 1);
+    }
+
+    private int dfs(int i, int j) {
+        if (i + 1 == j) return 0; // 只有两个点，无法组成三角形
+        if (memo[i][j] != -1) return memo[i][j];
+        int res = Integer.MAX_VALUE;
+        for (int k = i + 1; k < j; ++k) // 枚举顶点 k
+            res = Math.min(res, dfs(i, k) + dfs(k, j) + v[i] * v[j] * v[k]);
+        return memo[i][j] = res;
+    }
+
+
 }
