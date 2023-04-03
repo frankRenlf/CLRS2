48553de2f68aa7300599007add9adc652d9c78b2
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-03 09:21:02 +0800

    Create T1053.java

---
+++ clrs/src/leetcode/daily/T1053.java
@@ -0,0 +1,32 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/3 9:20
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1053 {
+    public int[] prevPermOpt1(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                int j = n - 1;
+                while (arr[j] >= arr[i] || arr[j] == arr[j - 1]) {
+                    j--;
+                }
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                break;
+            }
+        }
+        return arr;
+    }
+}
