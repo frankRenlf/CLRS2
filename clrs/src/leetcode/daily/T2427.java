4a4912b661005de24f5cd671f21d923dd05e9115
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-05 09:28:52 +0800

    Create T2427.java

---
+++ clrs/src/leetcode/daily/T2427.java
@@ -0,0 +1,25 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/5 9:28
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T2427 {
+    public int commonFactors(int a, int b) {
+        int ans = 0;
+        for (int x = 1; x <= Math.min(a, b); ++x) {
+            if (a % x == 0 && b % x == 0) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
