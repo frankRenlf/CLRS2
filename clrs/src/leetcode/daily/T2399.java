2e97059336520afc5ac9e343421842ab2d3caf49
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-09 09:44:37 +0800

    finish t2399

--- clrs/src/leetcode/daily/T2399.java
+++ clrs/src/leetcode/daily/T2399.java
@@ -1,5 +1,7 @@
 package leetcode.daily;
 
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +16,21 @@ package leetcode.daily;
  */
 public class T2399 {
     public boolean checkDistances(String s, int[] distance) {
+        int[] generate = new int[26];
+        Arrays.fill(generate, -1);
+        for (int i = 0; i < s.length(); i++) {
+            int pos = s.charAt(i) - 'a';
+            generate[pos] = generate[pos] == -1 ? i : i - generate[pos] - 1;
+        }
+        System.out.println(Arrays.toString(generate));
+        for (int i = 0; i < 26; i++) {
+            if (generate[i] != -1 && generate[i] != distance[i]) return false;
+        }
+        return true;
+    }
+    static T2399 t2399 = new T2399();
 
+    public static void main(String[] args) {
+        t2399.checkDistances("abaccb",new int[]{1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
     }
 }
