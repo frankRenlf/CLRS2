90336e209d9a025abd0b61ab0ba4db444bae6409
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-10 10:09:29 +0800

    Create T1019.java

---
+++ clrs/src/leetcode/daily/T1019.java
@@ -0,0 +1,46 @@
+package leetcode.daily;
+
+import leetcode.algorithm.ListNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/10 9:07
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1019 {
+    public int[] nextLargerNodes(ListNode head) {
+        List<Integer> ans = new ArrayList<Integer>();
+        Deque<int[]> stack = new ArrayDeque<int[]>();
+
+        ListNode cur = head;
+        int idx = -1;
+        while (cur != null) {
+            ++idx;
+            ans.add(0);
+            while (!stack.isEmpty() && stack.peek()[0] < cur.val) {
+                ans.set(stack.pop()[1], cur.val);
+            }
+            stack.push(new int[]{cur.val, idx});
+            cur = cur.next;
+        }
+
+        int size = ans.size();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; ++i) {
+            arr[i] = ans.get(i);
+        }
+        return arr;
+    }
+}
