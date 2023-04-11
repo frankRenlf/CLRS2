6c1aee5756a7d4504ee10e579eb29f51b4231c0b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-11 09:03:29 +0800

    Update T1041.java

--- clrs/src/leetcode/daily/T1041.java
+++ clrs/src/leetcode/daily/T1041.java
@@ -14,23 +14,23 @@ package leetcode.daily;
  */
 public class T1041 {
     public boolean isRobotBounded(String instructions) {
-        int[][] direc = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int direcIndex = 0;
+        int[][] direct = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int directIndex = 0;
         int x = 0, y = 0;
         int n = instructions.length();
         for (int idx = 0; idx < n; idx++) {
             char instruction = instructions.charAt(idx);
             if (instruction == 'G') {
-                x += direc[direcIndex][0];
-                y += direc[direcIndex][1];
+                x += direct[directIndex][0];
+                y += direct[directIndex][1];
             } else if (instruction == 'L') {
-                direcIndex += 3;
-                direcIndex %= 4;
+                directIndex += 3;
+                directIndex %= 4;
             } else {
-                direcIndex++;
-                direcIndex %= 4;
+                directIndex++;
+                directIndex %= 4;
             }
         }
-        return direcIndex != 0 || (x == 0 && y == 0);
+        return directIndex != 0 || (x == 0 && y == 0);
     }
 }
