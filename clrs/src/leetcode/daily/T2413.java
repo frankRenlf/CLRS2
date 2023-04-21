a8b620a1b3dd4717a1e399407e5108551ee2a56f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-21 09:08:23 +0800

    Create T2413.java

---
+++ clrs/src/leetcode/daily/T2413.java
@@ -0,0 +1,19 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/21 8:40
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T2413 {
+    public int smallestEvenMultiple(int n) {
+        return (n % 2 + 1) * n;
+    }
+}
