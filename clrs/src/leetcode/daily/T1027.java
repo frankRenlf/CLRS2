22b981e1f19c7c98ad0731c144c5b463a6c9dafe
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-22 08:27:02 +0800

    Create T1027.java

---
+++ clrs/src/leetcode/daily/T1027.java
@@ -0,0 +1,19 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/22 8:26
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description :
+ */
+public class T1027 {
+    public int longestArithSeqLength(int[] nums) {
+
+    }
+}
