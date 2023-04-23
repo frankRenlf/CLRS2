e9e68bf27adaab99eff7ba37f2ce3965df11766e
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-23 08:47:38 +0800

    Create T1105.java

---
+++ clrs/src/leetcode/daily/T1105.java
@@ -0,0 +1,16 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/23 8:47
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1105 {
+}
