229e3c99056b4344ab719906069acd7c84ed9bde
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-27 16:10:56 +0800

    Create T1048.java

---
+++ clrs/src/leetcode/daily/T1048.java
@@ -0,0 +1,32 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/27 16:10
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1048 {
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int res = 0;
+        for (String word : words) {
+            cnt.put(word, 1);
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                if (cnt.containsKey(prev)) {
+                    cnt.put(word, Math.max(cnt.get(word), cnt.get(prev) + 1));
+                }
+            }
+            res = Math.max(res, cnt.get(word));
+        }
+        return res;
+    }
+}
