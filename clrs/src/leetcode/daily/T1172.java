8ba801b9e458ce7ed97d3cb604ad5effe952bd8a
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-28 07:59:05 +0800

    Create T1172.java

---
+++ clrs/src/leetcode/daily/T1172.java
@@ -0,0 +1,16 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/4/28 7:58
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T1172 {
+}
