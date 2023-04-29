5c2a74c39461d6a148ef7f2c6d03c5a1128e48be
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-29 09:01:53 +0800

    Update T2423.java

--- clrs/src/leetcode/daily/T2423.java
+++ clrs/src/leetcode/daily/T2423.java
@@ -1,5 +1,10 @@
 package leetcode.daily;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,6 +19,27 @@ package leetcode.daily;
  */
 public class T2423 {
     public boolean equalFrequency(String word) {
-
+        int[] charCount = new int[26];
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            charCount[word.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (charCount[i] == 0) {
+                continue;
+            }
+            charCount[i]--;
+            HashSet<Integer> frequency = new HashSet<Integer>();
+            for (int f : charCount) {
+                if (f > 0) {
+                    frequency.add(f);
+                }
+            }
+            if (frequency.size() == 1) {
+                return true;
+            }
+            charCount[i]++;
+        }
+        return false;
     }
 }
