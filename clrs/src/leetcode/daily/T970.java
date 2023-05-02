79d93535eb7181c90e0a091d4646148ddcf5b5ad
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-02 08:19:02 +0800

    Update T970.java

--- clrs/src/leetcode/daily/T970.java
+++ clrs/src/leetcode/daily/T970.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @Package : leetcode.daily
  * @createTime : 2023/5/2 8:16
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
+ * @github : <a href="https://github.com/frankRenlf">...</a>
  * @Description :
  */
 public class T970 {
