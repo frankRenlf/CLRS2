a66f7c8beda072e0cbcf6bcf32316223ab0c7ffd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-02 08:17:01 +0800

    Create T970.java

---
+++ clrs/src/leetcode/daily/T970.java
@@ -0,0 +1,21 @@
+package leetcode.daily;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/5/2 8:16
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T970 {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+
+    }
+}
