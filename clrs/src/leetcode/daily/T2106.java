61e3b9f140112e88147469134ff8bd22135ab229
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-04 10:04:36 +0800

    Create T2106.java

---
+++ clrs/src/leetcode/daily/T2106.java
@@ -0,0 +1,39 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/5/4 10:04
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2106 {
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int left = 0;
+        int right = 0;
+        int n = fruits.length;
+        int sum = 0;
+        int ans = 0;
+        // 每次固定住窗口右边界
+        while (right < n) {
+            sum += fruits[right][1];
+            // 移动左边界
+            while (left <= right && step(fruits, startPos, left, right) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            ans = Math.max(ans, sum);
+            right++;
+        }
+        return ans;
+    }
+
+    public int step(int[][] fruits, int startPos, int left, int right) {
+        return Math.min(Math.abs(startPos - fruits[right][0]), Math.abs(startPos - fruits[left][0])) + fruits[right][0] - fruits[left][0];
+    }
+}
