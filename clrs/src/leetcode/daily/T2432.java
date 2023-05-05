20bb3e587cc57e0580736697b73d6c2a3159c51b
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-05 11:30:49 +0800

    Create T2432.java

---
+++ clrs/src/leetcode/daily/T2432.java
@@ -0,0 +1,19 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/5/5 11:30
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class T2432 {
+    public int hardestWorker(int n, int[][] logs) {
+    
+    }
+}
