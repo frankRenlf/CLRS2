ff5ca9cf8357ae5161f7069e65a6478ca7f5a577
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-06 07:54:33 +0800

    Create T1419.java

---
+++ clrs/src/leetcode/daily/T1419.java
@@ -0,0 +1,17 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/5/6 7:54
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description : medium
+ */
+public class T1419 {
+    public int minNumberOfFrogs(String croakOfFrogs) {}
+}
