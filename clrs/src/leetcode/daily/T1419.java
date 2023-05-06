dec29cf46b95b17cddd767f666c5979946039202
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-06 08:05:58 +0800

    Update T1419.java

--- clrs/src/leetcode/daily/T1419.java
+++ clrs/src/leetcode/daily/T1419.java
@@ -1,5 +1,8 @@
 package leetcode.daily;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -13,5 +16,36 @@ package leetcode.daily;
  * @Description : medium
  */
 public class T1419 {
-    public int minNumberOfFrogs(String croakOfFrogs) {}
+    public int minNumberOfFrogs(String croakOfFrogs) {
+        int n = croakOfFrogs.length();
+        if (n % 5 != 0) return -1;
+        Map<Character, Integer> map = new HashMap<>() {{
+            put('c', 0);
+            put('r', 1);
+            put('o', 2);
+            put('a', 3);
+            put('k', 4);
+        }};
+        int[] cnt = new int[4];
+        int cur = 0;
+        int sum = 0;
+        for (char c : croakOfFrogs.toCharArray()) {
+            int pos = map.get(c);
+            if (pos == 0) {
+                cnt[pos]++;
+                cur++;
+                sum = Math.max(sum, cur);
+            } else {
+                if (cnt[pos - 1] == 0) return -1;
+                cnt[pos - 1]--;
+                if (pos == 4) {
+                    cur--;
+                } else {
+                    cnt[pos]++;
+                }
+            }
+        }
+        if (cur > 0) return -1;
+        return sum;
+    }
 }
