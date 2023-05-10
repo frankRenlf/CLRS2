5c06efa17a5ea5ab5c036d5748bc536d5d1bfbd8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-10 08:31:18 +0800

    Update T1015.java

--- clrs/src/leetcode/daily/T1015.java
+++ clrs/src/leetcode/daily/T1015.java
@@ -9,26 +9,18 @@ package leetcode.daily;
  * @Package : leetcode.daily
  * @createTime : 2023/5/10 8:17
  * @Email : sc19lr@leeds.ac.uk
- * @github : https://github.com/frankRenlf
- * @Description :
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description : medium
  */
 public class T1015 {
-    public int smallestRepunitDivByK(int k) {
-        // 若 k 能被 2 或 5 整除，则无解，返回 -1
-        if (k % 2 == 0 || k % 5 == 0) {
+    public int smallestRepunitDivByK(int K) {
+        if (K % 2 == 0 || K % 5 == 0)
             return -1;
+        int i = 1;
+        for (int n = 1; n % K != 0; i++) {
+            n %= K;
+            n = n * 10 + 1;
         }
-        // 初始化余数为 1，表示一个数的最低位是 1
-        int resid = 1 % k, len = 1;
-        // 若余数不为 0，继续迭代
-        int ori = resid;
-        while (resid != 0) {
-            // 计算下一个数的余数，下一个数在当前余数后加一个 1
-            ori = ori * 10 + 1;
-            resid = (ori) % k;
-            len++;
-        }
-        // 返回数字 1 的最小重复次数
-        return len;
+        return i;
     }
 }
