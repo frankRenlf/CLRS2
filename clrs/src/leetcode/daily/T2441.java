cb502bb5b164bdb5f65b12d0046fe4e00e4a2251
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-05-13 07:36:57 +0800

    Create T2441.java

---
+++ clrs/src/leetcode/daily/T2441.java
@@ -0,0 +1,19 @@
+package leetcode.daily;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : clrs
+ * @Package : leetcode.daily
+ * @createTime : 2023/5/13 7:36
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : <a href="https://github.com/frankRenlf">...</a>
+ * @Description : easy
+ */
+public class T2441 {
+    public int findMaxK(int[] nums) {
+
+    }
+}
